7357eb127543061ec3da933051e74f1d66a50354
Nataliya R <n.rudenko@gmail.com>
2019-03-21 02:07:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,8 @@ class Backer
  end 
  
  def back_project(title)
-   @backed_projects << @@title
-    @backed_projects << @@backers
+   @backed_projects << @title
+    @backed_projects << @backers
   
  end 
  
